kantord/LibreLingo

fix(answer-corrector): allow for punctuation mistakes, but suggest correct form
fixes #580

workspaces/answer-corrector/index.js
@@ -1,29 +1,62 @@
 import levenshtein from "js-levenshtein"
 
-export default function evaluateAnswer({ validAnswers, answer }) {
+const id = x => x
+
+const ignorePunctuation = form => form.replace(/[!¡?¿,.]/g, "")
+
+const evaluateAnswerRaw = ({
+    validAnswers,
+    answer,
+    suggestor,
+    alwaysSuggest,
+    mapper
+}) => {
     let correct = false,
         suggestion = ""
 
     validAnswers.forEach(form => {
+        const mappedForm = mapper(form)
         if (
             levenshtein(
                 answer
                     .toLowerCase()
                     .replace(/^\s+|\s+$/g, "")
                     .replace(/\s+/g, " "),
-                form.toLowerCase()
+                mappedForm.toLowerCase()
             ) <= 1
         ) {
             correct = true
-            suggestion =
-        form
-            .replace(/^\s+|\s+$/g, "")
-            .replace(/\s+/g, " ")
-            .toLowerCase() === answer.toLowerCase()
-            ? ""
-            : `You made a small error. Correct spelling: ${form}`
+            suggestion = suggestor(form)
+            if ( !alwaysSuggest & (mappedForm
+                .replace(/^\s+|\s+$/g, "")
+                .replace(/\s+/g, " ")
+                .toLowerCase() === answer.toLowerCase())
+            ) {
+                suggestion = ""
+            }
         }
     })
 
     return { correct, suggestion }
+}
+
+export default function evaluateAnswer({ validAnswers, answer }) {
+    let result = evaluateAnswerRaw({
+        mapper: id,
+        validAnswers,
+        answer,
+        suggestor: form => `You made a small error. Correct spelling: ${form}`
+    })
+
+    if (!result.correct) {
+        result = evaluateAnswerRaw({
+            alwaysSuggest: true,
+            validAnswers,
+            mapper: ignorePunctuation,
+            answer,
+            suggestor: form => `Watch out for punctuation! Correct spelling: ${form}`
+        })
+    }
+
+    return result
 }

MPreston1/LibreLingoCommunity

workspaces/answer-corrector/index.js
@@ -1,29 +1,62 @@
 import levenshtein from "js-levenshtein"
 
-export default function evaluateAnswer({ validAnswers, answer }) {
+const id = x => x
+
+const ignorePunctuation = form => form.replace(/[!¡?¿,.]/g, "")
+
+const evaluateAnswerRaw = ({
+    validAnswers,
+    answer,
+    suggester,
+    alwaysSuggest,
+    mapper
+}) => {
     let correct = false,
         suggestion = ""
 
     validAnswers.forEach(form => {
+        const mappedForm = mapper(form)
         if (
             levenshtein(
                 answer
                     .toLowerCase()
                     .replace(/^\s+|\s+$/g, "")
                     .replace(/\s+/g, " "),
-                form.toLowerCase()
+                mappedForm.toLowerCase()
             ) <= 1
         ) {
             correct = true
-            suggestion =
-        form
-            .replace(/^\s+|\s+$/g, "")
-            .replace(/\s+/g, " ")
-            .toLowerCase() === answer.toLowerCase()
-            ? ""
-            : `You made a small error. Correct spelling: ${form}`
+            suggestion = suggester(form)
+            if ( !alwaysSuggest & (mappedForm
+                .replace(/^\s+|\s+$/g, "")
+                .replace(/\s+/g, " ")
+                .toLowerCase() === answer.toLowerCase())
+            ) {
+                suggestion = ""
+            }
         }
     })
 
     return { correct, suggestion }
+}
+
+export default function evaluateAnswer({ validAnswers, answer }) {
+    let result = evaluateAnswerRaw({
+        mapper: id,
+        validAnswers,
+        answer,
+        suggester: form => `You made a small error. Correct spelling: ${form}`
+    })
+
+    if (!result.correct) {
+        result = evaluateAnswerRaw({
+            alwaysSuggest: true,
+            validAnswers,
+            mapper: ignorePunctuation,
+            answer,
+            suggester: form => `Watch out for punctuation! Correct spelling: ${form}`
+        })
+    }
+
+    return result
 }