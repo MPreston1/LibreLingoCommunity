5c63bc271e7a1a7dc0e3523817197875bd7bf234
ggadt <giorgino.greg@yahoo.com>
2025-01-22 15:42:06 +0100

    fix: fixed navbar import

--- apps/web/src/settings.js
+++ apps/web/src/settings.js
@@ -1,19 +1,16 @@
-import settings from "../settings.json"
-import Cookies from "js-cookie"
+import settings from '../config/settings.json';
+import Cookies from 'js-cookie';
 
-const isDevelopment =
-  !process.env.NODE_ENV || process.env.NODE_ENV === "development"
+const isDevelopment = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
 
-const database = !isDevelopment
-  ? settings.database.production
-  : settings.database.development
+const database = !isDevelopment ? settings.database.production : settings.database.development;
 
-const authEnabled = Cookies.get("authEnabled") !== "false" || process.browser
+const authEnabled = Cookies.get('authEnabled') !== 'false' || process.browser;
 
 export default {
-  ...settings,
-  database,
-  features: {
-    authEnabled,
-  },
-}
+	...settings,
+	database,
+	features: {
+		authEnabled
+	}
+};
