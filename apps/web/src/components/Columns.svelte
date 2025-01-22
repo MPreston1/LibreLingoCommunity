1c47b7d9c440d37ab93621934ea03475cec0ba4d
ggadt <giorgino.greg@yahoo.com>
2025-01-22 15:28:52 +0100

    feat: imported various components

---
+++ apps/web/src/components/Columns.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	export let multiline = false;
+	export let reversed = false;
+</script>
+
+<div class="columns" class:is-multiline={multiline} class:is-reversed={reversed}>
+	<slot />
+</div>
+
+<style type="text/scss">
+	@use 'bulma/sass/utilities/_all';
+
+	@include until($tablet) {
+		.is-reversed {
+			flex-direction: column-reverse;
+			display: flex;
+		}
+	}
+</style>
---
+++ apps/web/src/components/Content.svelte
@@ -0,0 +1,10 @@
+<div class="content">
+	<slot />
+</div>
+
+<style type="text/scss">
+	@use 'bulma/sass/utilities/initial-variables.sass';
+	@use 'bulma/sass/utilities/derived-variables.sass';
+	@use 'bulma/sass/utilities/mixins.sass';
+	@use 'bulma/sass/elements/content.sass';
+</style>
