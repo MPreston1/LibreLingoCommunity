3b64478d0f580c9bacb34fbed6e2b8aaf57e1b27
ggadt <giorgino.greg@yahoo.com>
2025-01-22 13:33:41 +0100

    chore: imported utils folder

--- apps/web/src/utils/isBrowser.ts
+++ apps/web/src/utils/isBrowser.ts
@@ -2,6 +2,5 @@ import { browser } from '$app/environment';
 export default function isBrowser(): boolean {
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
-	//		return new Boolean(process.browser).valueOf();
-	return browser;
+	return new Boolean(process.browser).valueOf();
 }
