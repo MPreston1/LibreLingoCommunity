c12b179ed7aa7a1e7d7f175586e99196aadc8aca
ggadt <giorgino.greg@yahoo.com>
2025-01-22 13:33:41 +0100

    chore: imported utils folder

--- apps/web/src/utils/isBrowser.ts
+++ apps/web/src/utils/isBrowser.ts
@@ -1,5 +1,5 @@
 export default function isBrowser(): boolean {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  return new Boolean(process.browser).valueOf()
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	return new Boolean(process.browser).valueOf();
 }
