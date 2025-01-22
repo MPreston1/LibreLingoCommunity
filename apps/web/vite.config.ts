23048111c0a27e3a14995f5f57f0ca8bed823d48
ggadt <giorgino.greg@yahoo.com>
2025-01-22 15:35:32 +0100

    fix: improved config for letting the components loading starting from the src folder

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,5 +2,29 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	css: {
+		preprocessorOptions: {
+			sass: {
+				quietDeps: true,
+				silenceDeprecations: ['legacy-js-api'],
+				api: 'modern-compiler' // or 'modern'
+			},
+			scss: {
+				api: 'modern-compiler', // or 'modern'
+				quietDeps: true,
+				silenceDeprecations: ['legacy-js-api', 'import', 'global-builtin']
+			}
+		}
+	},
+	plugins: [sveltekit()],
+	server: {
+		fs: {
+			allow: [
+				// search up for workspace root
+				//searchForWorkspaceRoot('.'),
+				// your custom rules
+				'config'
+			]
+		}
+	}
 });
