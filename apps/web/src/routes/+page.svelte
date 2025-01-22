b3cb0f02f26503683df4115df9db228b6b0e53a1
ggadt <giorgino.greg@yahoo.com>
2025-01-22 15:02:37 +0100

    feat: added Hero and Backdrop component

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	import Hidden from '../components/Hidden.svelte';
 	import Translate from '../components/Translate.svelte';
 	import Stack from '../components/Stack.svelte';
-Added all stacks in home
+	import Hero from '../components/Hero/Hero.svelte';
 </script>
 
 <svelte:head>
@@ -13,58 +13,59 @@ Added all stacks in home
 </svelte:head>
 
 <main class="main-content">
-	<!--  <Page>
-    <Hero>
--->
-	<Stack direction="column" spacing="m" fullHeight justify="center">
-		<Stack spacing="m" direction="column" directionDesktop="row">
-			<Stack shrink={4}>
-				<!-- <Mascot shadow={false} glow={true} /> -->
-			</Stack>
-			<Stack direction="column" justify="center" shrink={2}>
-				<!--<Heading level={1}>
+	<!--  <Page>-->
+	<Hero>
+		<Stack direction="column" spacing="m" fullHeight justify="center">
+			<Stack spacing="m" direction="column" directionDesktop="row">
+				<Stack shrink={4}>
+					<!-- <Mascot shadow={false} glow={true} /> -->
+				</Stack>
+				<Stack direction="column" justify="center" shrink={2}>
+					<!--<Heading level={1}>
               <Translate key="index.subtitle">
                 A community-driven language-learning platform
               </Translate>
             </Heading>-->
+				</Stack>
+			</Stack>
+			<Stack justify="center">
+				<Button style="primary" size="large" href="course/spanish-from-english">
+					<Translate key="index.start_spanish_course">Start learning Spanish</Translate>
+				</Button>
 			</Stack>
 		</Stack>
-		<Stack justify="center">
-			<Button style="primary" size="large" href="course/spanish-from-english">
-				<Translate key="index.start_spanish_course">Start learning Spanish</Translate>
+
+		<Hidden>
+			<Button style="primary" href="course/german-from-english">Start learning German</Button>
+
+			<Button style="primary" href="course/french-from-english">Start learning French</Button>
+
+			<Button style="primary" href="course/bangla-from-english">Start learning Bangla</Button>
+
+			<Button style="primary" href="course/parsig-from-english"
+				>Start learning Middle Persian</Button
+			>
+
+			<Button style="primary" href="course/basque-from-english">Start learning Basque</Button>
+
+			<Button style="primary" href="course/ladino-from-english">
+				Start learning Ladino (for English speakers)
 			</Button>
-		</Stack>
-	</Stack>
 
-	<Hidden>
-		<Button style="primary" href="course/german-from-english">Start learning German</Button>
+			<Button style="primary" href="course/ladino-from-hebrew">
+				Start learning Ladino (for Hebrew speakers)
+			</Button>
 
-		<Button style="primary" href="course/french-from-english">Start learning French</Button>
+			<Button style="primary" href="course/ladino-from-spanish">
+				Start learning Ladino (for Spanish speakers)
+			</Button>
 
-		<Button style="primary" href="course/bangla-from-english">Start learning Bangla</Button>
-
-		<Button style="primary" href="course/parsig-from-english">Start learning Middle Persian</Button>
-
-		<Button style="primary" href="course/basque-from-english">Start learning Basque</Button>
-
-		<Button style="primary" href="course/ladino-from-english">
-			Start learning Ladino (for English speakers)
-		</Button>
-
-		<Button style="primary" href="course/ladino-from-hebrew">
-			Start learning Ladino (for Hebrew speakers)
-		</Button>
-
-		<Button style="primary" href="course/ladino-from-spanish">
-			Start learning Ladino (for Spanish speakers)
-		</Button>
-
-		<Button style="primary" href="course/houma-from-english">
-			Start learning Houma (for English speakers)
-		</Button>
-	</Hidden>
-	<!-- </Hero>
-    <Footer />
+			<Button style="primary" href="course/houma-from-english">
+				Start learning Houma (for English speakers)
+			</Button>
+		</Hidden>
+	</Hero>
+	<!-- <Footer />
   </Page> -->
 </main>
 
