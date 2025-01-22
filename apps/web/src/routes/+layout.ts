eb029a2a3b0bca7c4045eddce025cbd1edbebd20
ggadt <giorgino.greg@yahoo.com>
2025-01-22 13:32:36 +0100

    fix: fixed translations not working correctly

--- apps/web/src/routes/+layout.ts
+++ apps/web/src/routes/+layout.ts
@@ -3,7 +3,6 @@ import { browser } from '$app/environment';
 import '$lib/i18n'; // Import to initialize. Important :)
 import { locale, waitLocale } from 'svelte-i18n';
 import type { LayoutLoad } from './$types';
-import '@fortawesome/fontawesome-free/css/all.min.css'
 
 export const load: LayoutLoad = async () => {
 	if (browser) {
