2ba2692350aa1fd0bed81ce5ae9653c6e1671345
ggadt <giorgino.greg@yahoo.com>
2025-01-22 15:51:59 +0100

    chore: style and minors

--- apps/web/src/components/MarkDownPage.svelte
+++ apps/web/src/components/MarkDownPage.svelte
@@ -1,31 +1,31 @@
 <script lang="ts">
-  import NavBar from "../components/NavBar.svelte"
-  import Content from "lluis/Content.svelte"
+	import NavBar from 'components/NavBar.svelte';
+	import Content from 'components/Content.svelte';
 
-  export let readmeHTML: string
-  export let title: string | null
-  export let description: string | null = null
+	export let readmeHTML: string;
+	export let title: string | null;
+	export let description: string | null = null;
 </script>
 
 <svelte:head>
-  <title>{title}</title>
-  {#if description}
-    <meta name="description" content="{description}" />
-  {/if}
+	<title>{title}</title>
+	{#if description}
+		<meta name="description" content={description} />
+	{/if}
 </svelte:head>
 
 <NavBar />
 
 <section class="hero is-primary md-page">
-  <div class="hero-body">
-    <div class="container">
-      <h1 class="title">{title}</h1>
-      <div class="section box">
-        <slot />
-        <Content>
-          {@html readmeHTML}
-        </Content>
-      </div>
-    </div>
-  </div>
+	<div class="hero-body">
+		<div class="container">
+			<h1 class="title">{title}</h1>
+			<div class="section box">
+				<slot />
+				<Content>
+					{@html readmeHTML}
+				</Content>
+			</div>
+		</div>
+	</div>
 </section>
