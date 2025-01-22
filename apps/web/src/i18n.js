83b3d360fb902c71da44c60fbfaa48e3717fa335
ggadt <giorgino.greg@yahoo.com>
2025-01-22 13:28:08 +0100

    feat: adding translations

--- apps/web/src/i18n.js
+++ apps/web/src/i18n.js
@@ -1,14 +1,14 @@
-import { register, init } from "svelte-i18n"
+import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 
-register("en", () => require("./translation/en.json"))
-register("es", () => require("./translation/es.json"))
-register("fr", () => require("./translation/fr.json"))
-register("de", () => require("./translation/de.json"))
-register("it", () => require("./translation/it.json"))
-register("eo", () => require("./translation/eo.json"))
-register("pl", () => require("./translation/pl.json"))
+register('en', () => import('./translation/en.json'));
+register('es', () => import('./translation/es.json'));
+register('fr', () => import('./translation/fr.json'));
+register('de', () => import('./translation/de.json'));
+register('it', () => import('./translation/it.json'));
+register('eo', () => import('./translation/eo.json'));
+register('pl', () => import('./translation/pl.json'));
 
 init({
-  fallbackLocale: "en",
-  initialLocale: "en",
-})
+	fallbackLocale: 'en',
+	initialLocale: getLocaleFromNavigator()
+});
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,2 +1,20 @@
+<script lang="ts">
+  import { _, locale } from "svelte-i18n"
+
+  export let uiLanguage = 'it'
+  locale.set(uiLanguage)
+
+</script>
+<svelte:head>
+  <title>{$_("meta.title")}</title>
+  <meta name="description" content={$_("index.meta.description")} />
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+</div>
+
+<style lang="text/scss">
+
+</style>
