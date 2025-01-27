b42274fbb72cd94b19d2fe1747afb1ed40a18b41
ggadt <giorgino.greg@yahoo.com>
2025-01-27 15:50:14 +0100

    build: :building_construction: changed path in docker container for scripts

--- scripts/exportYamlCourse.sh
+++ scripts/exportYamlCourse.sh
@@ -2,8 +2,10 @@
 
 set -euo pipefail
 
+cd /LibreLingo/app
+
 echo -en "⏳ Exporting course $1"
-if pdm run export-course ./courses/"$1" ./apps/web/src/courses/"$1"; then
+if pdm run export-course /LibreLingo/courses/"$1" /LibreLingo/librelingo-web/src/courses/"$1"; then
 		echo -en "\r\033[K✅ Exported course $1"
 else
 		echo -en "\r⚠️  Couldn't export course $1"
